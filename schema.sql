692ad917f6f756930e43b1ca6fb61d7c7fb0a2a4
Thomas Levine <occurrence@thomaslevine.com>
2013-03-13 18:47:46 -0400

    aoeu

--- schema.sql
+++ schema.sql
@@ -1,12 +1,19 @@
 CREATE TABLE "tweets" (
-  "user" NOT NULL,
+  -- Username
+  -- http://www.mediabistro.com/alltwitter/tag/maximum-username-length-on-twitter
+  "user" VARCHAR(20) NOT NULL,
 
   -- 64-bit unsigned integers
   -- https://dev.twitter.com/docs/twitter-ids-json-and-snowflake
   -- http://dev.mysql.com/doc/refman/5.6/en/integer-types.html
   "id" BIGINT NOT NULL,
-  ??,
+
+  "huh?",
+
+  -- Dates without fractional seconds
   "date" TIMESTAMP(0),
+
+  -- The Tweet
   "text" VARCHAR(140),
   PRIMARY KEY ("id")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
