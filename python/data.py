6818404bff867b846ce13d6cffbca7ff71d6485f
Thomas Levine <occurrence@thomaslevine.com>
2013-03-14 18:47:30 -0400

    return

--- python/data.py
+++ python/data.py
@@ -4,3 +4,4 @@ def rds():
     'Connect to the data in Amazon RDS.'
     db = MySQLdb.connect(host="localhost", user="john", passwd="megajonhy", db="jonhydb")
     print('This is a connection. Read http://mysql-python.sourceforge.net/MySQLdb.html#mysqldb to see how to use it.')
+    return db
