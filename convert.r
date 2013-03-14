da9115b2fbb23f39e829fee326234db62cc32e64
Thomas Levine <occurrence@thomaslevine.com>
2013-03-14 18:43:41 -0400

    Symlink to /var/tweets; I'm out of space on /home

--- convert.r
+++ convert.r
@@ -4,7 +4,7 @@ source('r/data.r')
 library(RMySQL)
 con <- dbConnect(MySQL(), user="root", password="", dbname="tweets", host="localhost")
 
-for (spreadsheet in paste('tweets/tweets_ar', 1:12, '.txt')) {
-#  tweets <- tsv(spreadsheet)
-#  dbWriteTable(con, 'tweets', tweets, overwrite = F, append = T, row.names = F)
+for (spreadsheet in c('tweets/tweets_ar.txt', paste('tweets/tweets_ar', 1:12, '.txt', sep = ''))) {
+  tweets <- tsv(spreadsheet)
+  dbWriteTable(con, 'tweets', tweets, overwrite = F, append = T, row.names = F)
 }
