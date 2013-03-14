61e38fc26441963f70ecd5791f26a122368a3721
Thomas Levine <occurrence@thomaslevine.com>
2013-03-14 18:46:19 -0400

    loading 1 to 3

--- convert.r
+++ convert.r
@@ -4,7 +4,10 @@ source('r/data.r')
 library(RMySQL)
 con <- dbConnect(MySQL(), user="root", password="", dbname="tweets", host="localhost")
 
-for (spreadsheet in c('tweets/tweets_ar.txt', paste('tweets/tweets_ar', 1:12, '.txt', sep = ''))) {
+# spreadsheets <- c('tweets/tweets_ar.txt', paste('tweets/tweets_ar_', 1:12, '.txt', sep = ''))
+spreadsheets <- paste('tweets/tweets_ar_', 1:3, '.txt', sep = '')
+
+for (spreadsheet in spreadsheets) {
   tweets <- tsv(spreadsheet)
   dbWriteTable(con, 'tweets', tweets, overwrite = F, append = T, row.names = F)
 }
