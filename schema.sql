4c33a66f3f31588d668f629f87449756a69dd0d5
Thomas Levine <occurrence@thomaslevine.com>
2013-03-14 22:53:21 -0400

    remove import table

--- schema.sql
+++ schema.sql
@@ -1,13 +1,4 @@
-CREATE TABLE `tweets_import` (
-  `username` VARCHAR(20) NOT NULL,
-  `userid` INT NOT NULL,
-  `id` VARCHAR(60) NOT NULL,
-  `date` TIMESTAMP NOT NULL DEFAULT 0,
-  `text` VARCHAR(140) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `tweets` (
-  -- Username
   -- http://www.mediabistro.com/alltwitter/tag/maximum-username-length-on-twitter
   `username` VARCHAR(20) NOT NULL,
 
@@ -16,13 +7,9 @@ CREATE TABLE `tweets` (
 
   -- 64-bit unsigned integers
   -- https://dev.twitter.com/docs/twitter-ids-json-and-snowflake
-  -- http://dev.mysql.com/doc/refman/5.6/en/integer-types.html
   `id` BIGINT UNSIGNED NOT NULL,
 
-  -- `alwaysnull`,
-
   -- Dates without fractional seconds
-  -- `date` TIMESTAMP(0) NOT NULL,
   `date` TIMESTAMP NOT NULL DEFAULT 0,
 
   -- The Tweet
