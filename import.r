6e80eb59d0a221facb7f9b3b96fe29e82ebd46fa
Thomas Levine <occurrence@thomaslevine.com>
2013-03-15 01:17:36 -0400

    write csv

--- import.r
+++ import.r
@@ -6,14 +6,12 @@ library(RMySQL)
 con <- dbConnect(MySQL())
 
 spreadsheets <- c('tweets/tweets_ar.txt', paste('tweets/tweets_ar_', c(1:4,6,8), '.txt', sep = ''))
-for (spreadsheet in spreadsheets) {
-  cat(paste('Reading', spreadsheet, '\n'))
-  tweets.new <- tsv(spreadsheet)
-  if ('tweets' %in% ls()){
-    tweets <- rbind(tweets, tweets.new)
-  } else {
-    tweets <- tweets.new
-  }
-}
+for (spreadsheet.in in spreadsheets) {
+  cat(paste('Reading', spreadsheet.in, '\n'))
+  tweets <- tsv(spreadsheet.in)
 
-# Just write to csv because dbWriteTable is being weird
+  # Just write to csv because dbWriteTable is being weird
+  spreadsheet.out <- paste(spreadsheet, '.csv', sep = '')
+  cat(paste('Reading', spreadsheet.out, '\n'))
+  write.csv(tweets, row.names = F, file = spreadsheet.out)
+}
