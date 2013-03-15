a632700627511b0038a49f52eab01bcff66635dd
Thomas Levine <occurrence@thomaslevine.com>
2013-03-14 21:04:03 -0400

    feedback

--- import.r
+++ import.r
@@ -6,9 +6,11 @@ library(RMySQL)
 con <- dbConnect(MySQL())
 
 # spreadsheets <- c('tweets/tweets_ar.txt', paste('tweets/tweets_ar_', 1:12, '.txt', sep = ''))
-spreadsheets <- paste('tweets/tweets_ar_', 1:3, '.txt', sep = '')
+spreadsheets <- paste('tweets/tweets_ar_', 2:3, '.txt', sep = '')
 
 for (spreadsheet in spreadsheets) {
+  cat(paste('Reading', spreadsheet, 'from disk'))
   tweets <- tsv(spreadsheet)
+  cat(paste('Inserting', spreadsheet, 'into the database'))
   dbWriteTable(con, 'tweets', tweets, overwrite = F, append = T, row.names = F)
 }
