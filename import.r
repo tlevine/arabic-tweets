52a2415984147f8290f2303b0d872a4074cd5b4d
Thomas Levine <occurrence@thomaslevine.com>
2013-03-14 22:56:23 -0400

    filenames

--- import.r
+++ import.r
@@ -5,8 +5,7 @@ library(RMySQL)
 # con <- dbConnect(MySQL(), user="root", password="", dbname="tweets", host="endpoint")
 con <- dbConnect(MySQL())
 
-# spreadsheets <- c('tweets/tweets_ar.txt', paste('tweets/tweets_ar_', 1:12, '.txt', sep = ''))
-spreadsheets <- paste('tweets/tweets_ar_', 3, '.txt', sep = '')
+spreadsheets <- paste('tweets/tweets_ar.txt', 'tweets/tweets_ar_', c(1:4,6,8), '.txt', sep = '')
 
 # Make this faster?
 # http://dev.mysql.com/doc/refman/5.0/en/innodb-tuning.html
@@ -16,5 +15,5 @@ for (spreadsheet in spreadsheets) {
   cat(paste('Reading', spreadsheet, 'from disk\n'))
   tweets <- tsv(spreadsheet)
   cat(paste('Inserting', spreadsheet, 'into the database\n'))
-  dbWriteTable(con, 'tweets_import', tweets, overwrite = F, append = T, row.names = F)
+  dbWriteTable(con, 'tweets', tweets, overwrite = F, append = T, row.names = F)
 }
