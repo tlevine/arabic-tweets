b53059f9936ac95b8d12594afe480629d7544321
Thomas Levine <occurrence@thomaslevine.com>
2013-03-15 01:17:55 -0400

    oeu

--- import.r
+++ import.r
@@ -1,10 +1,6 @@
+#!/usr/bin/env Rscript
 # Convert the source TSV files to a bunch of nice formats.
 source('r/data.r')
-
-library(RMySQL)
-# con <- dbConnect(MySQL(), user="root", password="", dbname="tweets", host="endpoint")
-con <- dbConnect(MySQL())
-
 spreadsheets <- c('tweets/tweets_ar.txt', paste('tweets/tweets_ar_', c(1:4,6,8), '.txt', sep = ''))
 for (spreadsheet.in in spreadsheets) {
   cat(paste('Reading', spreadsheet.in, '\n'))
