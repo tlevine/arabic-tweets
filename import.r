6500abbcb66641d9a7baa8d8a8d1b9bea411f7d9
Thomas Levine <occurrence@thomaslevine.com>
2013-03-15 00:20:06 -0400

    remove newline

--- import.r
+++ import.r
@@ -5,6 +5,7 @@ library(RMySQL)
 # con <- dbConnect(MySQL(), user="root", password="", dbname="tweets", host="endpoint")
 con <- dbConnect(MySQL())
 
+
 spreadsheets <- c('tweets/tweets_ar.txt', paste('tweets/tweets_ar_', c(1:4,6,8), '.txt', sep = ''))
 
 for (spreadsheet in spreadsheets) {
