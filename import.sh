72478b7c7a8e9a990a7f9cbc7301ff7c945074cf
Thomas Levine <occurrence@thomaslevine.com>
2013-03-16 08:47:00 -0400

    infile

--- import.sh
+++ import.sh
@@ -4,4 +4,4 @@
 cat tweets/tweets_ar.txt.csv tweets/tweets_ar[1-9].txt.csv tweets/tweets_ar1[01].txt.csv > tweets/tweets.csv
 
 # Import
-mysqlimport tweets --local --default-character-set=utf8 --fields-enclosed-by=\" --fields-terminated-by=, --lines-terminated-by=\\n --columns=username,userid,id,date,text tweets/tweets.csv
+mysql --local-infile=1 < import.sql
