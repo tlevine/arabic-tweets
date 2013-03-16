8966ae5f9058885388cbd366583079015d088d59
Thomas Levine <occurrence@thomaslevine.com>
2013-03-16 08:06:40 -0400

    headers

--- random-subset.py
+++ random-subset.py
@@ -5,6 +5,7 @@ import random
 t_in = open('tweets/tweets.csv')
 t_out = open('tweets/tweets-sample.csv', 'a')
 
+t_out.write('username,userid,id,date,text\n')
 for line in t_in:
     if random.uniform(0,1) < (20000 / 95000000):
         t_out.write(line)
