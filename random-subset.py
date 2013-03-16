4aa1c664053d00a9bd6d04c9b0340ca85df4fbd9
Thomas Levine <occurrence@thomaslevine.com>
2013-03-16 08:12:27 -0400

    oeu

--- random-subset.py
+++ random-subset.py
@@ -2,10 +2,18 @@
 'Select a random subset of lines.'
 import random
 
+# Expected sample size
+EXPECTED_N = 20000.0
+
+t_out = open('tweets/tweets-sample.csv', 'w')
+t_out.write('username,userid,id,date,text\n')
+t_out.close()
+
 t_in = open('tweets/tweets.csv')
 t_out = open('tweets/tweets-sample.csv', 'a')
 
-t_out.write('username,userid,id,date,text\n')
+cutoff = (EXPECTED_N / 95000000.0)
 for line in t_in:
-    if random.uniform(0,1) < (20000 / 95000000):
+    if random.uniform(0,1) < cutoff:
         t_out.write(line)
+t_out.close()
