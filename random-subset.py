ffe3e59ccfa3596759d55c00516533c463e0da87
Thomas Levine <occurrence@thomaslevine.com>
2013-03-16 10:01:03 -0400

    subset sizes

--- random-subset.py
+++ random-subset.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python2
 'Select a random subset of lines.'
 import random
+import sys
 
-# Expected sample size
-EXPECTED_N = 20000.0
+expected_n = float(sys.argv[1])
 
-t_out = open('tweets/tweets-sample.csv', 'w')
+t_out = open('tweets/tweets-sample-%d.csv' % int(expected_n), 'w')
 t_out.write('username,userid,id,date,text\n')
 t_out.close()
 
 t_in = open('tweets/tweets.csv')
-t_out = open('tweets/tweets-sample.csv', 'a')
+t_out = open('tweets/tweets-sample-%d.csv' % int(expected_n), 'a')
 
-cutoff = (EXPECTED_N / 95000000.0)
+cutoff = (expected_n / 95000000.0)
 for line in t_in:
     if random.uniform(0,1) < cutoff:
         t_out.write(line)
