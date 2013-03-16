15eb58803eea3c585ce2caed415cba3867f1ff0d
Thomas Levine <occurrence@thomaslevine.com>
2013-03-16 00:26:37 -0400

    stack in order

--- import.sh
+++ import.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cat tweets/tweets_ar*.txt.csv > tweets/tweets.csv
+# Stack in order
+cat tweets/tweets_ar.txt.csv tweets/tweets_ar[1-9].txt.csv tweets/tweets_ar1[01].txt.csv > tweets/tweets.csv
+
+# Import
 mysqlimport tweets --local --default-character-set=utf8 --fields-enclosed-by=\" --fields-terminated-by=, --lines-terminated-by=\\n --columns=username,userid,id,date,text tweets/tweets.csv
